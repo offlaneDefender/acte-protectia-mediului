8df0fc1af6842329c63b1292bd8bf839fadf0fa0
offlaneDefender <github@mikko.mozmail.com>
2024-07-07 23:09:39 +0300

    Nav attempt fix #2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://offlanedefender.github.io",
+  base: "/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
